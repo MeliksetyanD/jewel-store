459675c3214c216592f7346da59a23d504739854
DavMel <davmelsim.01@gmail.com>
2024-10-10 02:02:52 +0400

    header create

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import About from './pages/About/About.jsx'
+import Account from './pages/Account/Account.jsx'
+import Blog from './pages/Blog/Blog.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx'
+import Home from './pages/Home/Home.jsx'
+import Layout from './pages/layout/Layout.jsx'
+import { store } from './store/store.js'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <Home /> },
+			{
+				path: '/blog',
+				element: <Blog />,
+			},
+			{
+				path: '/about',
+				element: <About />,
+			},
+			{
+				path: '/account',
+				element: <Account />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+		],
+	},
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</StrictMode>
 )
