04c26a300dbf6a028097d66e1ed18472d4362931
DavMel <davmelsim.01@gmail.com>
2024-10-15 22:40:11 +0400

    product page created

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 import styles from './Product.module.css'
 import img from './ddd.png'
-const Product = () => {
+const Product = ({ id = 1 }) => {
 	return (
-		<Link className={styles.product} to='/product/:id'>
+		<Link className={styles.product} to={`/product/${id}`}>
 			<img src={img} alt='jewel' width={377} height={380} />
 			<h3>Hal Earrings</h3>
 			<h4>$ 68,00</h4>
