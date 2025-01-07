f2cec74c592c24a450301b3e3b8372fed8ffc53c
Ararat <aromiq1@gmail.com>
2025-01-08 02:05:21 +0300

    update put method

---
+++ server/utils/connect.js
@@ -0,0 +1,12 @@
+import Sequelize from 'sequelize'
+
+const NAME = 'shoppe'
+const USER = 'root'
+const PASSWORD = '12345678'
+
+const sequelize = new Sequelize(NAME, USER, PASSWORD, {
+	host: 3306,
+	dialect: 'mysql',
+})
+
+export default sequelize
